6bae014e5dcadd931f15bb8a557dd6de44626f58
Fred <frederik.koenig@posteo.de>
2025-01-05 18:37:32 +0100

    improved the if no category

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,22 +7,6 @@ export const dynamic = "force-dynamic";
 
 export default async function Home() {
   const categories = await getCategories();
-  if (categories.length === 0) {
-    console.log("No categories found");
-    return (
-      <div className="bg-[url('/kollage-mama.jpg')] min-h-screen w-full animate-float">
-        <h1 className="text-5xl flex justify-center p-4 font-bold">
-          <p className="bg-zinc-50 rounded-2xl p-3 opacity-80">
-            Erinnerungsbuch
-          </p>
-        </h1>
-        <div className="grid grid-cols-2 gap-3 m-4">
-          <p>No categorys found</p>
-          <AddCategory />
-        </div>
-      </div>
-    );
-  }
 
   return (
     <div className="bg-[url('/kollage-mama.jpg')] min-h-screen w-full animate-float">
@@ -30,18 +14,22 @@ export default async function Home() {
         <p className="bg-zinc-50 rounded-2xl p-3 opacity-80">Erinnerungsbuch</p>
       </h1>
       <div className="grid grid-cols-2 gap-3 m-4">
-        {categories.map((category) => {
-          console.log("Category:", category);
-          return (
-            <Link
-              href={`/category/${category.id}`}
-              key={category.id}
-              className="p-3 bg-zinc-100 rounded-xl text-4xl opacity-80 flex justify-center"
-            >
-              <p className="">{category.name}</p>
-            </Link>
-          );
-        })}
+        {categories && categories.length > 0 ? (
+          categories.map((category) => {
+            console.log("Category:", category);
+            return (
+              <Link
+                href={`/category/${category.id}`}
+                key={category.id}
+                className="p-3 bg-zinc-100 rounded-xl text-4xl opacity-80 flex justify-center"
+              >
+                <p className="">{category.name}</p>
+              </Link>
+            );
+          })
+        ) : (
+          <div>no categories found</div>
+        )}
         <AddCategory />
       </div>
     </div>
