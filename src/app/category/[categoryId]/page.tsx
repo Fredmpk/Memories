743f30aadc91b24b66a71c62b005468f16481c2d
Fred <frederik.koenig@posteo.de>
2025-01-06 00:34:13 +0100

    changed memories order

--- src/app/category/[categoryId]/page.tsx
+++ src/app/category/[categoryId]/page.tsx
@@ -34,24 +34,29 @@ export default async function Category({
 
       <Carousel className="w-[80vw] bg-zinc-100 opacity-95 m-5">
         <CarouselContent>
-          {memories.map((memory, index) => (
-            <CarouselItem key={index} className="w-[90vw]">
-              <div className="p-1">
-                <Card className="h-[70vh]">
-                  <CardContent className="flex flex-col  items-center justify-center p-6 h-full">
-                    <h1 className="text-3xl font-bold mb-3">{memory.title}</h1>
+          {memories
+            .slice()
+            .reverse()
+            .map((memory, index) => (
+              <CarouselItem key={index} className="w-[90vw]">
+                <div className="p-1">
+                  <Card className="h-[70vh]">
+                    <CardContent className="flex flex-col  items-center justify-center p-6 h-full">
+                      <h1 className="text-3xl font-bold mb-3">
+                        {memory.title}
+                      </h1>
 
-                    <textarea
-                      name="Text"
-                      id="index"
-                      placeholder={memory.text}
-                      className="text-2xl italic flex-1 w-full placeholder-black placeholder:italic p-2"
-                    ></textarea>
-                  </CardContent>
-                </Card>
-              </div>
-            </CarouselItem>
-          ))}
+                      <textarea
+                        name="Text"
+                        id="index"
+                        placeholder={memory.text}
+                        className="text-2xl italic flex-1 w-full placeholder-black placeholder:italic p-2"
+                      ></textarea>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
         </CarouselContent>
         <CarouselPrevious className="bg-zinc-50" />
         <CarouselNext className="bg-zinc-50 " />
