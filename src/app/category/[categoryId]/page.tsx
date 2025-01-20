856cb50e300675560079fff6896188e5ccc5013b
Fred <frederik.koenig@posteo.de>
2025-01-20 17:29:22 +0100

    installed swiper

--- src/app/category/[categoryId]/page.tsx
+++ src/app/category/[categoryId]/page.tsx
@@ -11,6 +11,9 @@ import {
 import getMemories from "@/lib/utils/get-memories";
 import AddMemory from "@/components/add-memory";
 import DeleteMemory from "@/components/delete-memory";
+import dynamic from "next/dynamic";
+import SimpleSwiper from "@/components/swiper";
+import MemoriesSwiper from "@/components/swiper";
 
 export default async function Category({
   params,
@@ -30,36 +33,8 @@ export default async function Category({
       <h1 className="text-4xl m-4 font-bold text-center bg-black text-white rounded-2xl p-3 opacity-80 whitespace-normal break-words max-w-full">
         {category.name}
       </h1>
-
-      <AddMemory categoryId={resolvedParams.categoryId} />
-
-      <Carousel className="w-[80vw] bg-black text-white  opacity-95 m-5">
-        <CarouselContent>
-          {reversedMemories.map((memory, index) => (
-            <CarouselItem key={memory.id} className="w-[90vw]">
-              <div className="p-1">
-                <Card className="h-[72vh]">
-                  <CardContent className="flex flex-col  items-center justify-center px-6 pt-3 h-full bg-black text-white ">
-                    <h1 className="text-3xl font-bold mb-10">{memory.title}</h1>
-
-                    <textarea
-                      name="Text"
-                      id="index"
-                      placeholder={memory.text}
-                      readOnly
-                      className="text-2xl italic flex-1 w-full bg-zinc-800 placeholder-white placeholder:italic p-3"
-                    ></textarea>
-                    <h2 className="text-3xl font-bold my-3">{memory.author}</h2>
-                    <DeleteMemory memoryId={memory.id} />
-                  </CardContent>
-                </Card>
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        <CarouselPrevious className="bg-zinc-50" />
-        <CarouselNext className="bg-zinc-50 " />
-      </Carousel>
+      <AddMemory categoryId={category.id} />
+      <MemoriesSwiper memories={reversedMemories} />
     </div>
   );
 }
---
+++ src/components/swiper.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Card, CardContent } from "@/components/ui/card";
+import DeleteMemory from "./delete-memory";
+
+type Memory = {
+  id: string;
+  title: string | null;
+  text: string;
+  author: string | null;
+  categoryId: string | null;
+};
+
+export default function MemoriesSwiper({ memories }: { memories: Memory[] }) {
+  return (
+    <div className="w-[80vw]">
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        {memories.map((memory, index) => (
+          <SwiperSlide key={memory.id} className="w-[90vw]">
+            <div className="p-1">
+              <Card className="h-[72vh]">
+                <CardContent className="flex flex-col  items-center justify-center px-6 pt-3 h-full bg-black text-white ">
+                  <h1 className="text-3xl font-bold mb-10">{memory.title}</h1>
+
+                  <textarea
+                    name="Text"
+                    id="index"
+                    placeholder={memory.text}
+                    readOnly
+                    className="text-2xl italic flex-1 w-full bg-zinc-800 placeholder-white placeholder:italic p-3"
+                  ></textarea>
+                  <h2 className="text-3xl font-bold my-3">{memory.author}</h2>
+                  <DeleteMemory memoryId={memory.id} />
+                </CardContent>
+              </Card>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
