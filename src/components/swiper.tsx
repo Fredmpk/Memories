c1b710b7e2f43d955dd7cf0f910aea5640e2f5ea
Fred <frederik.koenig@posteo.de>
2025-01-20 18:10:21 +0100

    adjusted styling slider

--- src/components/swiper.tsx
+++ src/components/swiper.tsx
@@ -1,6 +1,10 @@
 "use client";
+
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 import { Card, CardContent } from "@/components/ui/card";
 import DeleteMemory from "./delete-memory";
 
@@ -16,10 +20,24 @@ export default function MemoriesSwiper({ memories }: { memories: Memory[] }) {
   return (
     <div className="w-[80vw]">
       <Swiper
+        modules={[Navigation, Pagination]}
         spaceBetween={50}
         slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
         onSlideChange={() => console.log("slide change")}
         onSwiper={(swiper) => console.log(swiper)}
+        style={
+          {
+            "--swiper-navigation-color": "#ffffff",
+            "--swiper-pagination-color": "#32CD32" as any,
+            "--swiper-pagination-bullet-inactive-color": "#999999" as any,
+            "--swiper-pagination-bullet-inactive-opacity": "1" as any,
+            "--swiper-pagination-bullet-size": "12px",
+            "--swiper-pagination-bullet-horizontal-gap": "6px",
+            "--swiper-pagination-bullet-vertical-gap": "6px",
+          } as React.CSSProperties
+        }
       >
         {memories.map((memory, index) => (
           <SwiperSlide key={memory.id} className="w-[90vw]">
@@ -36,6 +54,7 @@ export default function MemoriesSwiper({ memories }: { memories: Memory[] }) {
                     className="text-2xl italic flex-1 w-full bg-zinc-800 placeholder-white placeholder:italic p-3"
                   ></textarea>
                   <h2 className="text-3xl font-bold my-3">{memory.author}</h2>
+
                   <DeleteMemory memoryId={memory.id} />
                 </CardContent>
               </Card>
